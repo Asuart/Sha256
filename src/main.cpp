0620cdc6e9a93a78c94ac3ce7ef856541825e9be
asuart <asuart@yandex.ru>
2024-01-10 11:08:59 +0300

    Cleanup bad code and format.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include "sha256.h"
 
- class Debug{
+class Debug {
      template <class T>
-     static void PrintAsBin(const T val){
-          u32 size = sizeof(T) * 8;
-          for (u32 i = 0; i < size; i++)
-               std::cout << (u32)((val >> (size - i)) & 1);
+     static void PrintAsBin(const T val) {
+          uint32_t size = sizeof(T) * 8;
+          for (uint32_t i = 0; i < size; i++)
+               std::cout << (uint32_t)((val >> (size - i)) & 1);
           std::cout << std::endl;
      }
 };
 
-s32 main(int argc, char *argv[]) {
-
-     if (argc < 2) { std::cout << "No arguments specified. Use -help for more information." << std::endl; return 0;}
+int main(int argc, char* argv[]) {
+     if (argc < 2) { std::cout << "No arguments specified. Use -help for more information." << std::endl; return 0; }
 
      std::string arg1 = std::string(argv[1]);
-     
+
      if (arg1 == "-h" || arg1 == "-help" || arg1 == "?" || arg1 == "-?" || arg1 == "--help") {
           std::cout << "This command can get any number of parameters and will compute hash for each of them. The format if following:" << std::endl;
           std::cout << "1) use \"string\" to compute hash of a string" << std::endl;
@@ -30,24 +29,25 @@ s32 main(int argc, char *argv[]) {
 
      bool awaitFile = false;
      bool muteWarnings = false;
-     for (u32 i = 1; i < argc; i++) {
+     for (uint32_t i = 1; i < argc; i++) {
           std::string arg = std::string(argv[i]);
-          if (arg == "-m") { muteWarnings = true; continue;}
-          if (awaitFile && arg == "-f") { if (!muteWarnings) std::cout << "Warning: no file path specified for argument -f at index: " << i - 1 << std::endl; continue;}
-          if (arg == "-f") { awaitFile = true; continue;}
-          if (awaitFile){
+          if (arg == "-m") { muteWarnings = true; continue; }
+          if (awaitFile && arg == "-f") { if (!muteWarnings) std::cout << "Warning: no file path specified for argument -f at index: " << i - 1 << std::endl; continue; }
+          if (arg == "-f") { awaitFile = true; continue; }
+          if (awaitFile) {
                std::fstream reader(arg.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
-               if (!reader.is_open()) { if (!muteWarnings) std::cout << "Could not open file: " << arg << std::endl; return 1;}
-               u32 inputSize = reader.tellg();
+               if (!reader.is_open()) { if (!muteWarnings) std::cout << "Could not open file: " << arg << std::endl; return 1; }
+               uint32_t inputSize = reader.tellg();
                reader.seekg(reader.beg);
-               u8 *fileData = new u8[inputSize];
-               reader.read((s8 *)fileData, inputSize);
+               uint8_t* fileData = new uint8_t[inputSize];
+               reader.read((char*)fileData, inputSize);
                reader.close();
                Sha256::GetHash(fileData, inputSize).Print();
                delete[] fileData;
-          } else {
-               u8 *fileData = (u8 *)argv[i];
-               u32 inputSize = std::string(argv[i]).size();
+          }
+          else {
+               uint8_t* fileData = (uint8_t*)argv[i];
+               uint32_t inputSize = std::string(argv[i]).size();
                Sha256::GetHash(fileData, inputSize).Print();
           }
           awaitFile = false;
--- src/sha256.h
+++ src/sha256.h
@@ -3,22 +3,12 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <stdlib.h>
 #include <fstream>
 
-typedef char s8;
-typedef short s16;
-typedef int s32;
-typedef long long s64;
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
+struct Sha256Value {
+     uint32_t subHashes[8];
 
-struct Sha256Value
-{
-     u32 subHashes[8];
-     Sha256Value(){
+     Sha256Value() {
           subHashes[0] = 0x6A09E667;
           subHashes[1] = 0xBB67AE85;
           subHashes[2] = 0x3C6EF372;
@@ -28,35 +18,39 @@ struct Sha256Value
           subHashes[6] = 0x1F83D9AB;
           subHashes[7] = 0x5BE0CD19;
      }
-     void Print(){
+
+     void Print() {
           std::cout << std::hex;
-          for (u32 i = 0; i < 8; i++)
-               for (u32 j = 0; j < 8; j++)
+          for (uint32_t i = 0; i < 8; i++) {
+               for (uint32_t j = 0; j < 8; j++) {
                     std::cout << ((subHashes[i] >> (28 - 4 * j)) & 0xf);
+               }
+          }
           std::cout << std::dec << std::endl;
      }
 };
 
-class Sha256{
-     static u32 Sigma0(u32 val){
+class Sha256 {
+     static inline uint32_t Sigma0(uint32_t val) {
           return ((_rotr(val, 7)) ^ (_rotr(val, 18))) ^ (val >> 3);
      }
-     static u32 Sigma1(u32 val){
+
+     static inline uint32_t Sigma1(uint32_t val) {
           return ((_rotr(val, 17)) ^ (_rotr(val, 19))) ^ (val >> 10);
      }
 
-     static u32 UpSigma0(u32 val){
+     static inline uint32_t UpSigma0(uint32_t val) {
           return ((_rotr(val, 2)) ^ (_rotr(val, 13))) ^ (_rotr(val, 22));
      }
 
-     static u32 UpSigma1(u32 val){
+     static inline uint32_t UpSigma1(uint32_t val) {
           return ((_rotr(val, 6)) ^ (_rotr(val, 11))) ^ (_rotr(val, 25));
      }
 
 public:
-     static Sha256Value GetHash(const u8 *data, u32 size){
-          const u32 k[64] =
-              {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+     static Sha256Value GetHash(const uint8_t* data, uint32_t size) {
+          const uint32_t k[64] = {
+               0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
                0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
                0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
                0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -71,41 +65,55 @@ public:
                0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
                0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
                0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-               0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+               0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+          };
 
           Sha256Value hash = Sha256Value();
 
-          u64 sizeBit = size * 8;
-          u32 extendedSizeBit = sizeBit + 8 + 64;
-          u32 alignedSizeBit = extendedSizeBit + (512 - (extendedSizeBit % 512));
-          u32 blocksCount = alignedSizeBit / 512;
+          uint64_t sizeBit = size * 8;
+          uint32_t extendedSizeBit = sizeBit + 8 + 64;
+          uint32_t alignedSizeBit = extendedSizeBit + (512 - (extendedSizeBit % 512));
+          uint32_t blocksCount = alignedSizeBit / 512;
 
-          for (u32 i = 0; i < blocksCount; i++){
-               u8 buf[64];
-               u32 w[64];
-               for (u32 j = 0, l = i * 64; j < 64; j++, l++){
+          for (uint32_t i = 0; i < blocksCount; i++) {
+               uint8_t buf[64];
+               uint32_t w[64];
+               for (uint32_t j = 0, l = i * 64; j < 64; j++, l++) {
                     if (l < size) buf[j] = data[l];
                     else if (l == size) buf[j] = (1 << 7);
                     else buf[j] = 0;
                }
-               if (i == (blocksCount - 1))
-                    for (u32 i = 0; i < 8; i++) buf[63 - i] = (sizeBit >> (8 * i)) & 0xff;
+               if (i == (blocksCount - 1)) {
+                    for (uint32_t i = 0; i < 8; i++) {
+                         buf[63 - i] = (sizeBit >> (8 * i)) & 0xff;
+                    }
+               }
 
-               for (u32 j = 0; j < 64; j++) ((u8 *)w)[j] = buf[j + 3 - ((j & 3) << 1)];
-               for (u32 j = 16; j < 64; j++) w[j] = w[j - 16] + Sigma0(w[j - 15]) + w[j - 7] + Sigma1(w[j - 2]);
+               for (uint32_t j = 0; j < 64; j++) {
+                    ((uint8_t*)w)[j] = buf[j + 3 - ((j & 3) << 1)];
+               }
+               for (uint32_t j = 16; j < 64; j++) {
+                    w[j] = w[j - 16] + Sigma0(w[j - 15]) + w[j - 7] + Sigma1(w[j - 2]);
+               }
 
-               u32 _h[8];
-               for (u32 i = 0; i < 8; i++) _h[i] = hash.subHashes[i];
-               for (u32 j = 0; j < 64; j++){
-                    u32 ch = (_h[4] & _h[5]) ^ ((~_h[4]) & _h[6]);
-                    u32 maj = (_h[0] & _h[1]) ^ (_h[0] & _h[2]) ^ (_h[1] & _h[2]);
-                    u32 temp1 = _h[7] + UpSigma1(_h[4]) + ch + k[j] + w[j];
-                    u32 temp2 = UpSigma0(_h[0]) + maj;
-                    for (int i = 7; i > 0; i--) _h[i] = _h[i - 1];
+               uint32_t _h[8];
+               for (uint32_t i = 0; i < 8; i++) {
+                    _h[i] = hash.subHashes[i];
+               }
+               for (uint32_t j = 0; j < 64; j++) {
+                    uint32_t ch = (_h[4] & _h[5]) ^ ((~_h[4]) & _h[6]);
+                    uint32_t maj = (_h[0] & _h[1]) ^ (_h[0] & _h[2]) ^ (_h[1] & _h[2]);
+                    uint32_t temp1 = _h[7] + UpSigma1(_h[4]) + ch + k[j] + w[j];
+                    uint32_t temp2 = UpSigma0(_h[0]) + maj;
+                    for (int i = 7; i > 0; i--) {
+                         _h[i] = _h[i - 1];
+                    }
                     _h[4] += temp1;
                     _h[0] = temp1 + temp2;
                }
-               for (u32 i = 0; i < 8; i++) hash.subHashes[i] += _h[i];
+               for (uint32_t i = 0; i < 8; i++) {
+                    hash.subHashes[i] += _h[i];
+               }
           }
           return hash;
      }
